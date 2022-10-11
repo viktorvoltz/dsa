3dd2bebe84a8725aa98eae03ff4c4b0bb92fbc8b
viktorvoltz <vctrchinyeaka@gmail.com>
2022-10-11 08:49:38 +0100

    recursion

---
+++ recursion/BinarySearch.java
@@ -0,0 +1,33 @@
+package recursion;
+
+import java.math.BigInteger;
+
+class BinarySearch{
+
+    public static boolean binarySearch(int[] data, int target, int low, int high){
+        if(low > high){
+            return false;
+        }else{
+            int mid = (low + high) / 2;
+            if(target == data[mid]){
+                return true;
+            }else if(target < data[mid]){
+                return binarySearch(data, target, low, mid - 1);
+            }else{
+                return binarySearch(data, target, mid + 1, high);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int LENGTH = 500;
+        int[] data = new int[LENGTH];
+        for(int i = 0; i < data.length; i++){
+            data[i] = i;
+        }
+        long start = System.currentTimeMillis( ); 
+        boolean result = binarySearch(data, 300, 0, data.length - 1);
+        long end = System.currentTimeMillis( );
+        long elapsed = end - start;
+        System.out.println(result + " in:: " + elapsed);
+    }
+}
