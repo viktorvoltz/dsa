d23f343f1deaf4553c2a003ffe9d44048f1a6e6e
viktorvoltz <vctrchinyeaka@gmail.com>
2022-10-25 10:29:26 +0100

    binary

--- recursion/BinarySearch.java
+++ recursion/BinarySearch.java
@@ -1,30 +1,45 @@
 
+class BinarySearch {
 
-class BinarySearch{
-
-    public static boolean binarySearch(int[] data, int target, int low, int high){
-        if(low > high){
-            return false;
-        }else{
+    public static boolean binarySearchIterative(int[] data, int target) {
+        int low = 0;
+        int high = data.length - 1;
+        while (low <= high) {
             int mid = (low + high) / 2;
-            if(target == data[mid]){
+            if (target == data[mid])
                 return true;
-            }else if(target < data[mid]){
+            else if (target < data[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return false;
+    }
+
+    public static boolean binarySearch(int[] data, int target, int low, int high) {
+        if (low > high) {
+            return false;
+        } else {
+            int mid = (low + high) / 2;
+            if (target == data[mid]) {
+                return true;
+            } else if (target < data[mid]) {
                 return binarySearch(data, target, low, mid - 1);
-            }else{
+            } else {
                 return binarySearch(data, target, mid + 1, high);
             }
         }
     }
+
     public static void main(String[] args) {
         int LENGTH = 500;
         int[] data = new int[LENGTH];
-        for(int i = 0; i < data.length; i++){
+        for (int i = 0; i < data.length; i++) {
             data[i] = i;
         }
-        long start = System.currentTimeMillis( ); 
+        long start = System.currentTimeMillis();
         boolean result = binarySearch(data, 300, 0, data.length - 1);
-        long end = System.currentTimeMillis( );
+        long end = System.currentTimeMillis();
         long elapsed = end - start;
         System.out.println(result + " in:: " + elapsed);
     }
